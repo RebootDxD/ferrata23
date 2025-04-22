cf9365e4dc219a6fdcb362bcdd9830483c94355f
Andrew <potapira3844@mail.ru>
2025-04-22 21:14:25 +0300

    New

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.temp_water_slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        fade: true,
+        speed: 1000,
+        draggable: false,
+        accessibility: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+    });
+    $('.advantages_slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        accessibility: false,
+        draggable: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+    })
+});
+
